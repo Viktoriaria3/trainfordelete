e8a21603bcbda44a2bec9f94cf01d821a034a01c
Viktoriaria3 <137815390+Viktoriaria3@users.noreply.github.com>
2025-03-14 17:11:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const burger = document.querySelector(".burger-menu");
+  const navMenu = document.querySelector(".header-list");
+
+  burger.addEventListener("click", function () {
+      navMenu.classList.toggle("active");
+      burger.classList.toggle("active");
+  });
+
+  document.addEventListener("click", function (event) {
+      if (!burger.contains(event.target) && !navMenu.contains(event.target)) {
+          navMenu.classList.remove("active");
+          burger.classList.remove("active");
+      }
+  });
+});
