594816da254278ab95d116ca114efa351e02821d
Viktoriaria3 <137815390+Viktoriaria3@users.noreply.github.com>
2025-03-29 11:01:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const burger = document.querySelector(".burger-menu");
-  const navMenu = document.querySelector(".header-list");
+   const burger = document.querySelector(".burger-menu");
+   const navMenu = document.querySelector(".header_right--mobile--list");
 
-  burger.addEventListener("click", function () {
-      navMenu.classList.toggle("active");
-      burger.classList.toggle("active");
-  });
+   burger.addEventListener("click", function () {
+       navMenu.classList.toggle("active");
+       burger.classList.toggle("active");
+   });
 
-  document.addEventListener("click", function (event) {
-      if (!burger.contains(event.target) && !navMenu.contains(event.target)) {
-          navMenu.classList.remove("active");
-          burger.classList.remove("active");
-      }
-  });
+   document.addEventListener("click", function (event) {
+       if (!burger.contains(event.target) && !navMenu.contains(event.target)) {
+           navMenu.classList.remove("active");
+           burger.classList.remove("active");
+       }
+   });
 });
